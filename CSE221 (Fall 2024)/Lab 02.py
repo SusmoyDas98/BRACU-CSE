30d4da592c7597e7224db7c1d54f4d617a1d71f5
Susmoy Das <susmoydas998@gmail.com>
2024-12-31 11:16:43 +0600

    Lab 02.py

--- CSE221 (Fall 2024)/Lab 02.py
+++ CSE221 (Fall 2024)/Lab 02.py
@@ -1,5 +1,8 @@
-#Lab Assignment 2
-#Task 1
+'''  Lab Assignment 2  ''' 
+
+
+'''Task 1''' 
+
 with  open('input1.txt','r') as file1, open('output1.txt', 'w') as file2:
     N, S = map(int, file1.readline().strip().split(" "))
     lst = list(map(int, file1.readline().strip().split()))
@@ -21,8 +24,11 @@ with  open('input1.txt','r') as file1, open('output1.txt', 'w') as file2:
 #------------------------------------------------------------------------------------------------------------------------------
 
 
-#Task 2
-#A
+''' Task 2 '''
+
+
+''' A '''
+
 with  open('input2.txt','r') as file1, open('output2.txt', 'w') as file2:
     arr = list(file1.readlines())
     l1,lst1, l2, lst2 = arr
@@ -35,7 +41,9 @@ with  open('input2.txt','r') as file1, open('output2.txt', 'w') as file2:
     file2.write(f"{' '.join(final)}")
 
 
-#B
+''' B '''
+
+
 with  open('input2.txt','r') as file1, open('output2.txt', 'w') as file2:
     arr = list(file1.readlines())
     l1,lst1, l2, lst2 = arr
@@ -61,7 +69,9 @@ with  open('input2.txt','r') as file1, open('output2.txt', 'w') as file2:
 #------------------------------------------------------------------------------------------------------------------------------
 
 
-#Task 3
+''' Task 3 '''
+
+
 with open('input3.txt','r') as file1, open('output3.txt','w') as file2:
     N = int(file1.readline().strip())
     dic, end = {},[]
@@ -83,8 +93,11 @@ with open('input3.txt','r') as file1, open('output3.txt','w') as file2:
 #------------------------------------------------------------------------------------------------------------------------------
 
 
-#Task 4
-# A
+''' Task 4 '''
+
+''' A '''
+
+
 import math
 with open('input4.txt','r') as file1, open('output4.txt','w') as file2:
     N, M = map(int, file1.readline().strip().split())
